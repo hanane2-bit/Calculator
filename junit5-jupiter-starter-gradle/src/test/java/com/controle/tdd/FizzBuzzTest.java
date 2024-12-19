37e9e62dba2793dc13005110facce5aa15678be7
Hanane <hananemoubaker1@gmail.com>
2024-12-19 17:16:57 +0100

    cycle 5: FizzBuzz_de(2)->'2'

--- junit5-jupiter-starter-gradle/src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -52,6 +52,34 @@ public class FizzBuzzTest {
         assertEquals("FizzBuzz", result);
     }
 
+    @Test
+    void testFizzBuzz2() {
+        // Given
+        int input = 2;
+
+        // When
+        String result = FizzBuzz_de(input);
+
+        // Then
+        assertEquals("2", result);
+    }
+/*
+    // Test paramétré (1 à 20) avec "Given-When-Then"
+    @Test
+    void testFizzBuzzParametrized() {
+        // Given
+        String[] expectedResults = {
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"
+        };
+
+        // When & Then
+        for (int i = 1; i <= 20; i++) {
+            String result = FizzBuzz.de(i);
+            // Then
+            assertEquals(expectedResults[i - 1], result, "Error at i = " + i);
+        }
+    }*/
 
 
 
